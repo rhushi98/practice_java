b1103938f0c947dda8964fcc094329d5ebe2f060
rhushi98 <rhushimangasule@gmail.com>
2024-09-16 06:50:37 -0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,7 +1,13 @@
 public class main {
     public static void main(String[] arguments) {
-        Person p = new Person("Rhushi","26","Male");
+        Person p = new Person("Rhushi",26,"Male");
         System.out.println(p.getname()+","+p.getage()+","+p.getgender());
+        p.setage(24);
+        p.display();
+
+        Person p1 = new Person("yogi",21);
+        p1.display();
+
 
 
     }
